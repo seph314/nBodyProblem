147f10a3f62b323d536fe229323e2c24dd2a865e
Anders <andpet52@kth.se>
2018-02-16 23:31:05 +0100

    more work on Simulate

--- src/Simulate.java
+++ src/Simulate.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+
 public class Simulate {
 
     private Body[] bodies;
     private int dt;
+//    private PointsEx pointsEx;
 
     /**
      * Constructor
+     *
      * @param bodies is our array of bodies
      * @param dt     is the increase in time
      */
@@ -16,22 +20,23 @@ public class Simulate {
     /**
      * Simulates what happens to the bodies over time
      */
-    public Body[] time() {
+    public void time() {
 
         Vector[] forces = new Vector[bodies.length]; /* a new force array with the length of tge bodies array */
         for (int i = 0; i < bodies.length; i++) {
             forces[i] = new Vector(new double[2]);
         }
 
-        for (int i=0; i<bodies.length; i++)
+        for (int i = 0; i < bodies.length; i++)
             for (int j = 0; j < bodies.length; j++) {
                 forces[i] = forces[i].add(bodies[j].calculateForces(bodies[i])); /* calculate an array of forces */
             }
 
-        for (int i=0; i<bodies.length; i++)
+        for (int i = 0; i < bodies.length; i++) {
             bodies[i].movePoints(forces[i], dt); /* move all points */
-
-        return bodies; /* returns the bodies array containing the new values */
+            System.out.println("\nBody " + i + "\nposition: " + Arrays.toString(bodies[i].getPosition()));
+            System.out.println("velocity: " + Arrays.toString(bodies[i].getVelocity()));
+//            pointsEx = new PointsEx(bodies); /* try to draw it graphically */
+        }
     }
-
 }
